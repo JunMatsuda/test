d17de98f966ef5f553abc144df8d848dbe39838d
JunMatsuda <matsuda@ogu.ac.jp>
2022-11-18 20:57:01 +0900

    add Webgazer

---
+++ javascripts/getCurrentPrediction.js
@@ -0,0 +1,59 @@
+﻿webgazer.begin();
+
+var mstorage = sessionStorage;
+var mid = 0;
+var kstorage = sessionStorage;
+var kid = 0;
+
+const mlocalforage = localforage.createInstance({
+  name : 'MouseEvent', 
+});
+const klocalforage = localforage.createInstance({
+  name : 'KeyEvent', 
+});
+
+document.addEventListener('keypress', keypress_event);
+
+window.onclick = async function(e){
+  //マウスカーソルの座標を取得
+  var mx = e.clientX;
+  var my = e.clientY;
+
+  //視点の座標を取得
+  var prediction = await webgazer.getCurrentPrediction();
+  if (prediction) {
+    var gx = prediction.x;
+    var gy = prediction.y;
+  }
+  //console.log('mx:', mx, 'my:', my);
+  //console.log('gx:', gx, 'gy:', gy);
+  mid++;
+  var mdata = {
+    mx: mx,
+    my: my,
+    gx: gx,
+    gy: gy
+  }
+  mstorage.setItem('mdata'+mid, JSON.stringify(mdata));
+  mlocalforage.setItem('mdata'+mid, mdata);
+}
+
+async function keypress_event(e) {
+  //押されたキーのコードを取得
+  var key = e.code;
+
+  //視点の座標を取得
+  var prediction = await webgazer.getCurrentPrediction();
+  if (prediction) {
+    var gx = prediction.x;
+    var gy = prediction.y;
+  }
+  //console.log('key:', key, 'gx:', gx, 'gy:', gy);
+  kid++;
+  var kdata = {
+    gx: gx,
+    gy: gy
+  }
+  kstorage.setItem('kdata'+kid, JSON.stringify(kdata));
+  klocalforage.setItem('kdata'+kid, kdata);
+}
---
+++ javascripts/setGazeListener.js
@@ -0,0 +1,10 @@
+﻿let data;
+
+webgazer.setGazeListener(function(data, elapsedTime) {
+  if (data == null) {
+    return;
+  }
+  var xprediction = data.x; //these x coordinates are relative to the viewport
+  var yprediction = data.y; //these y coordinates are relative to the viewport
+  console.log(elapsedTime, xprediction); //elapsed time is based on time since begin was called
+}).begin();
